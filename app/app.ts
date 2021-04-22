aa096619e7100f7bfa4a9efe09d57949785db1ca
plam <vvillbes@gmail.com>
2021-04-22 17:16:11 +0300

    04_09 Demo: Taking Advantage of Function Types

--- app/app.ts
+++ app/app.ts
@@ -4,7 +4,8 @@ function startGame() {
     let playerName: string | undefined = getInputValue('playername');
     logPlayer(playerName);
 
-    postScore(100, playerName);
+    postScore(80, playerName);
+    postScore(-5, "Tom");
 }
 
 function logPlayer(name: string = 'MultiMath Player'): void {
@@ -24,11 +25,24 @@ function getInputValue(elementID: string): string | undefined {
 }
 
 function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+    let logger: (value: string) => void;
+
+    if (score < 0) {
+        logger = logError;
+    } else {
+        logger = logMessage;
+    }
+
     const scoreElement: HTMLElement | null = document.getElementById('postedScores');
     scoreElement!.innerText = `${score} - ${playerName}`;
+
+    logger(`Score: ${score}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
 
-const logMessage = (message: string) => console.log(message);
-logMessage('Welcome to MultiMath!');
+const logMessage = (message: string) => console.log(`Message: ${message}`);
+
+function logError(err: string): void {
+    console.error(`Error: ${err}`);
+}
